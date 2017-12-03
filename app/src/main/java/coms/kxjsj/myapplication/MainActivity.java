d3774eb8f75eb2c4176d98949c7208c7b28142f7
While1true <893825883@qq.com>
2017-12-03 12:57:19 +0800

    dragview

--- app/src/main/java/coms/kxjsj/myapplication/MainActivity.java
+++ app/src/main/java/coms/kxjsj/myapplication/MainActivity.java
@@ -3,37 +3,60 @@ package coms.kxjsj.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toast toast;
+    int[]colors={0xffffff00,0xff00ff00,0xff00ffff,0xff0000ff,0xff666666,0xff234578};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ViewDrag viewDrag = (ViewDrag) findViewById(R.id.viewDrag);
-        viewDrag.setSettingViewCallBack(new ViewDrag.SettingViewCallBack() {
-            @Override
-            void startSetting(View view) {
-                /**
-                 * 刚放手
-                 */
-            }
+        List<String>list=new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            list.add("这是第"+i+"项");
+        }
+        viewDrag.setAdapter(new ViewDrag.DragCardAdapter<String>(list,5) {
 
             @Override
-            void endSetting(View view) {
-                /**
-                 * 放手后动画结束
-                 */
-                if(toast==null)
-                toast = Toast.makeText(view.getContext(), "移动到最后了", Toast.LENGTH_LONG);
-                else {
-                    toast.setText("移动到最后了");
+            public View getView(int i, String str, View conventView, ViewGroup viewGroup) {
+                if(conventView==null){
+                    conventView=getLayoutInflater().inflate(R.layout.ss,viewGroup,false);
                 }
-                toast.show();
+                TextView tv=conventView.findViewById(R.id.tv);
+                tv.setText(str);
+                tv.setBackgroundColor(colors[i%colors.length]);
+                return conventView;
             }
         });
+//        viewDrag.setSettingViewCallBack(new ViewDrag.SettingViewCallBack() {
+//            @Override
+//            void startSetting(View view) {
+//                /**
+//                 * 刚放手
+//                 */
+//            }
+//
+//            @Override
+//            void endSetting(View view) {
+//                /**
+//                 * 放手后动画结束
+//                 */
+//                if(toast==null)
+//                toast = Toast.makeText(view.getContext(), "移动到最后了", Toast.LENGTH_LONG);
+//                else {
+//                    toast.setText("移动到最后了");
+//                }
+//                toast.show();
+//            }
+//        });
     }
 }
--- app/src/main/java/coms/kxjsj/myapplication/ViewDrag.java
+++ app/src/main/java/coms/kxjsj/myapplication/ViewDrag.java
@@ -1,19 +1,22 @@
 package coms.kxjsj.myapplication;
 
 import android.content.Context;
+import android.database.DataSetObservable;
+import android.database.DataSetObserver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.DateSorter;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,15 +29,26 @@ public class ViewDrag extends FrameLayout{
     private ViewDragHelper viewDragHelper;
     private ViewDragHelper.Callback callback;
 
+    private List<View> attchedView=new ArrayList<>();
 
-    List<View>allViews=new ArrayList<>();
+    private DragCardAdapter adapter;
+    private final DataSetObserver observer=new DataSetObserver() {
+        @Override
+        public void onChanged() {
+            handleViews();
+        }
 
+        @Override
+        public void onInvalidated() {
+            super.onInvalidated();
+        }
+    };
     /**
      * 标记settleCapturedViewAt开始
      */
     private boolean startSetting;
     private boolean startSettingBack;
-    private View releasedChild;
+    int currentFront;
 
     public ViewDrag(@NonNull Context context) {
         this(context,null);
@@ -49,11 +63,36 @@ public class ViewDrag extends FrameLayout{
         init();
     }
 
+    public void setAdapter(DragCardAdapter adapter){
+        this.adapter=adapter;
+        adapter.registerDataSetObserver(observer);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void handleViews(){
+        if(adapter==null||adapter.getCount()==0){
+            return;
+        }
+        removeAllViews();
+        attchedView.clear();
+        int cachedSize = adapter.getCachedSize();
+        for (int i = currentFront+cachedSize-1; i >=0; i--) {
+            View view = adapter.getView(i%adapter.getCount(),null, this);
+            attchedView.add(view);
+            addView(view);
+        }
+        for (int i = 0; i < attchedView.size(); i++) {
+            View view=attchedView.get(i);
+            view.setTranslationY(-(getChildCount()-i-1)*dp2px(10));
+            view.setScaleX(1f-0.3f*(getChildCount()-i)/getChildCount());
+            view.setScaleY(1f-0.3f*(getChildCount()-i)/getChildCount());
+        }
+    }
     private void init() {
         callback = new ViewDragHelper.Callback() {
             @Override
             public boolean tryCaptureView(View child, int pointerId) {
-                return child==allViews.get(allViews.size()-1);
+                return child==attchedView.get(attchedView.size()-1)||(attchedView.size()>1&&startSetting&&child==attchedView.get(attchedView.size()-2));
             }
 
             @Override
@@ -82,7 +121,7 @@ public class ViewDrag extends FrameLayout{
             @Override
             public void onViewCaptured(View capturedChild, int activePointerId) {
                 if(settingViewCallBack!=null){
-                    settingViewCallBack.captureView(capturedChild);
+                    settingViewCallBack.captureView(capturedChild,currentFront);
                 }
             }
 
@@ -101,7 +140,7 @@ public class ViewDrag extends FrameLayout{
                     viewDragHelper.settleCapturedViewAt(left, measuredHeight/2- releasedChild.getMeasuredHeight()/2);
                     startSetting = true;
                     if (settingViewCallBack != null) {
-                        settingViewCallBack.startSetting(releasedChild);
+                        settingViewCallBack.startSetting(releasedChild,currentFront);
                     }
                 }else{
                     releasedChild.setRotation(0);
@@ -152,13 +191,13 @@ public class ViewDrag extends FrameLayout{
             @Override
             public int clampViewPositionVertical(View child, int top, int dy) {
 //                System.out.println("top"+top+"--"+dy);
-                int centre = (getMeasuredHeight() - getPaddingTop() - getPaddingBottom()) / 2 - child.getMeasuredHeight() / 2;
-                if(top<centre-dp2px(80)){
-                    top=centre-dp2px(80);
-                }else if(top>centre+dp2px(80)){
-                    top=centre+dp2px(80);
-                }
-                return top;
+//                int centre = (getMeasuredHeight() - getPaddingTop() - getPaddingBottom()) / 2 - child.getMeasuredHeight() / 2;
+//                if(top<centre-dp2px(80)){
+//                    top=centre-dp2px(80);
+//                }else if(top>centre+dp2px(80)){
+//                    top=centre+dp2px(80);
+//                }
+                return clampBoundary(child,top,1);
             }
         };
         viewDragHelper = ViewDragHelper.create(this, 1, callback);
@@ -172,6 +211,7 @@ public class ViewDrag extends FrameLayout{
 //        } catch (IllegalAccessException e) {
 //            e.printStackTrace();
 //        }
+
     }
 
     private int clampBoundary(View view,int current,int orentation){
@@ -214,17 +254,19 @@ public class ViewDrag extends FrameLayout{
                 invalidate();
             } else {
                 startSetting=false;
-                View frontview = allViews.get(allViews.size() - 1);
+                View frontview = attchedView.get(attchedView.size() - 1);
                 frontview.setRotation(0);
                 if(settingViewCallBack!=null){
-                    settingViewCallBack.endSetting(frontview);
+                    settingViewCallBack.endSetting(frontview,currentFront);
                 }
-                allViews.remove(frontview);
-                allViews.add(0,frontview);
+                currentFront++;
+                attchedView.remove(frontview);
                 removeView(frontview);
-                addView(frontview,0);
-                for (int i = 0; i < allViews.size(); i++) {
-                    allViews.get(i).setTranslationY(-(allViews.size()-i-1)*dp2px(10));
+                View view = adapter.getView((currentFront+attchedView.size()) % adapter.getCount(), frontview, this);
+                attchedView.add(0,view);
+                addView(view,0);
+                for (int i = 0; i < attchedView.size(); i++) {
+                    attchedView.get(i).setTranslationY(-(attchedView.size()-i-1)*dp2px(10));
                     getChildAt(i).setScaleX(1f-0.3f*(getChildCount()-i)/getChildCount());
                     getChildAt(i).setScaleY(1f-0.3f*(getChildCount()-i)/getChildCount());
                 }
@@ -235,7 +277,7 @@ public class ViewDrag extends FrameLayout{
                 invalidate();
             }else{
                 startSettingBack=false;
-//                View frontview = allViews.get(allViews.size() - 1);
+//                View frontview = attchedView.get(attchedView.size() - 1);
 //                frontview.setRotation(0);
             }
         }
@@ -252,34 +294,26 @@ public class ViewDrag extends FrameLayout{
          * 开始退回
          * @param view
          */
-        void startSetting(View view){}
+        void startSetting(View view,int position){}
 
         /**
          * 结束退回
          * @param view
          */
-        void endSetting(View view){}
+        void endSetting(View view,int position){}
 
         /**
          * view捕获了
          * @param view
          */
-        void captureView(View view){}
+        void captureView(View view,int position){}
     }
 
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        allViews.clear();
-        for (int i = 0; i < getChildCount(); i++) {
-            allViews.add(getChildAt(i));
-            getChildAt(i).setTranslationY(-(getChildCount()-i-1)*dp2px(10));
-            getChildAt(i).setScaleX(1f-0.3f*(getChildCount()-i)/getChildCount());
-            getChildAt(i).setScaleY(1f-0.3f*(getChildCount()-i)/getChildCount());
-//            getChildAt(i).setScaleX(1f/(getChildCount()-i-1));
-        }
-    }
+}
 
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
@@ -290,4 +324,48 @@ public class ViewDrag extends FrameLayout{
     private int dp2px(float dp){
        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,getResources().getDisplayMetrics());
     }
+    
+    public static abstract class DragCardAdapter<T> extends BaseAdapter{
+        List<T> list;
+        int cachedSize=7;
+       
+        public DragCardAdapter(List<T> list,int cachedSize){
+            this.list=list;
+            this.cachedSize=cachedSize;
+        }
+
+        public List<T> getList() {
+            return list;
+        }
+
+        public int getCachedSize() {
+            return cachedSize;
+        }
+
+        public void stList(List<T> list){
+            this.list=list;
+        }
+        @Override
+        public int getCount() {
+            return list==null?0:list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public  View getView(int i, View conventView, ViewGroup viewGroup){
+            return getView(i,list.get(i),conventView,viewGroup);
+        }
+
+        public abstract View getView(int i,T t, View conventView, ViewGroup viewGroup);
+    }
+            
 }
